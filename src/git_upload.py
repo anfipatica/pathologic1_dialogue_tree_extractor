7f6d7a5d54e427079738a5ea0099a887f18a0dbd
anfi <anfi@anfi>
2025-09-13 15:57:24 +0200

    uploading to github from python script...

--- src/git_upload.py
+++ src/git_upload.py
@@ -1,11 +1,25 @@
 import git
+import utils.colors as colors
+import os
 
 def	upload_to_github():
 	repo: git.Repo
 	origin: git.Remote
 
-	repo = git.Repo(".")
-	origin = repo.remote("origin")
-	repo.git.add(".")
-	repo.index.commit("uploading to github from python script...")
-	origin.push()
+	print(f"{colors.GREEN}\n\n-> Uploading the changes to github.")
+	try:
+		repo = git.Repo(".")
+		origin = repo.remote("origin")
+		repo.git.add(".")
+		repo.index.commit("uploading to github from python script...")
+		family = os.fork()
+		state: bool = False
+
+		if (family == 0):
+			while (state == False):
+				print(state)
+		else:
+			origin.push()
+			state = True
+	except:
+		print(f"{colors.RED}Something went wrong while uploading the changes to github.{colors.STD}")
