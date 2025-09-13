915d4b9e56a00f879a6ee2cd36aea2a7f18e8c39
anfi <anfi@anfi>
2025-09-13 15:47:59 +0200

    uploading to github from python script...

--- src/git_upload.py
+++ src/git_upload.py
@@ -1,6 +1,11 @@
 import git
 
 def	upload_to_github():
+	repo: git.Repo
+	origin: git.Remote
+
 	repo = git.Repo(".")
+	origin = repo.remote("origin")
 	repo.git.add(".")
-	repo.index.commit("probando gitpython")
+	repo.index.commit("uploading to github from python script...")
+	origin.push()
